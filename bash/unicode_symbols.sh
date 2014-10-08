309fc898a13dd6fdffcfa973b38b9e286a992b31
Marcelo Carlos <me@marcelocarlos.com>
2014-10-08 20:06:44 +0100

    adding unicode symbols

---
+++ bash/unicode_symbols.sh
@@ -0,0 +1,18 @@
+# more symbols at: http://en.wikibooks.org/wiki/Unicode/List_of_useful_symbols
+# full list at http://www.utf8-chartable.de/unicode-utf8-table.pl (use hex (0x))
+CHECK_MARK="\xe2\x9c\x93"      # \u2713
+BOLD_CHECK_MARK="\xe2\x9c\x94" # \u2714
+CROSS_MARK="\xe2\x9c\x95"      # \u2715
+BOLD_CROSS_MARK="\xe2\x9c\x96" # \u2716
+
+LEFT_ARROW="\xe2\x86\x90"  # \u2190
+UP_ARROW="\xe2\x86\x91"    # \u2191
+RIGHT_ARROW="\xe2\x86\x92" # \u2192
+DOWN_ARROW="\xe2\x86\x93"  # \u2193
+
+BLACK_STAR="\xe2\x98\x85"  # \2605
+WHITE_STAR="\xe2\x98\x86"  # \2606
+
+echo -e $CHECK_MARK
+echo -e $CROSS_MARK
+echo -e $BLACK_STAR
